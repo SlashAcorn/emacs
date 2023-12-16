a40235e82c242b569f22bd94068ea05481325f0f
Nicholas <nhe@air.net.au>
2023-12-16 17:16:17 +1100

    remove custom-set-faces that are in init.el

--- custom.el
+++ custom.el
@@ -10,16 +10,9 @@
  '(custom-safe-themes
    '("b54376ec363568656d54578d28b95382854f62b74c32077821fdfd604268616a" default))
  '(package-selected-packages
-   '(nerd-icons-ibuffer org-bullets toc-org elfeed elfeed-dashboard elfeed-tube elfeed-web elcord auto-complete lsp-ivy lsp-mode impatient-mode impatient-showdown sudo-edit typescript-mode dirvish prescient ivy-prescient auto-package undo-fu-session undo-fu smartparens harpoon zen-mode magit minimap vterm-toggle dashboard neotree nerd-icons-completion nerd-icons-dired nerd-icons-ivy-rich dired-rainbow use-package bind-key counsel-fd counsel-notmuch counsel-projectile counsel-spotify counsel-web notmuch notmuch-addr notmuch-labeler notmuch-maildir projectile vterm fireplace 2048-game pacmacs markdown-mode gdscript-mode treemacs-all-the-icons treemacs all-the-icons-ivy all-the-icons-ivy-rich all-the-icons doom-modeline ivy-rich emojify no-littering counsel beacon consult helpful avy hydra swiper ivy doom-themes rainbow-delimiters which-key auto-package-update comment-dwim-2 ws-butler)))
+   '(rainbow-mode nerd-icons-ibuffer org-bullets toc-org elfeed elfeed-dashboard elfeed-tube elfeed-web elcord auto-complete lsp-ivy lsp-mode impatient-mode impatient-showdown sudo-edit typescript-mode dirvish prescient ivy-prescient auto-package undo-fu-session undo-fu smartparens harpoon zen-mode magit minimap vterm-toggle dashboard neotree nerd-icons-completion nerd-icons-dired nerd-icons-ivy-rich dired-rainbow use-package bind-key counsel-fd counsel-notmuch counsel-projectile counsel-spotify counsel-web notmuch notmuch-addr notmuch-labeler notmuch-maildir projectile vterm fireplace 2048-game pacmacs markdown-mode gdscript-mode treemacs-all-the-icons treemacs all-the-icons-ivy all-the-icons-ivy-rich all-the-icons doom-modeline ivy-rich emojify no-littering counsel beacon consult helpful avy hydra swiper ivy doom-themes rainbow-delimiters which-key auto-package-update comment-dwim-2 ws-butler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrainsMono Nerd Font" :foundry "JB" :slant normal :weight normal :height 113 :width expanded))))
- '(org-document-title ((t (:height 2.0))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.75))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.25))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
