cab7d2c6b445635db74b5d5080402592eb1bc98c
Nicholas <nhe@air.net.au>
2023-12-16 18:16:50 +1100

    add another rss feed

--- elfeed/feeds.el
+++ elfeed/feeds.el
@@ -1,4 +1,5 @@
 (defvar elfeed-feeds-alist
   '(("https://ericmurphy.xyz/index.xml" blog dev)
     ("https://sive.rs/en.atom" blog)
-    ("https://lukesmith.xyz/index.xml" blog)))
+    ("https://lukesmith.xyz/index.xml" blog)
+    ("https://systemcrafters.net/rss/news.xml")))
